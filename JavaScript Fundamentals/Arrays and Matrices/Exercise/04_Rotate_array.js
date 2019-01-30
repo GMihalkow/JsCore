1f803526aaca0d18378b9bb410013f027653cc8f
GMihalkow <32070470+GMihalkow@users.noreply.github.com>
2019-01-30 16:52:09 +0200

    Found a better way to finish the problem Rorate Array.

--- JavaScript Fundamentals/Arrays and Matrices/Exercise/04_Rotate_array.js
+++ JavaScript Fundamentals/Arrays and Matrices/Exercise/04_Rotate_array.js
@@ -1,13 +1,9 @@
 function solve(inputArr){
     let rotationsCount = +inputArr.pop();
-    if(rotationsCount === NaN || rotationsCount >= 260000){
-
-    }
-    else{
-        for (let index = 0; index < rotationsCount; index++) {
-            let firstElement = inputArr.pop();
-            inputArr.unshift(firstElement);
-        }
+    
+    for (let index = 0; index < +rotationsCount % inputArr.length; index++) {
+        let firstElement = inputArr.pop();
+        inputArr.unshift(firstElement);
     }
     
     console.log(inputArr.join(" "));
