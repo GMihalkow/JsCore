fd1544a984f85dbdf10e300e3eae6c453b11cd68
GMihalkow <georgimihalkov@abv.bg>
2019-03-26 00:37:04 +0200

    Reworked the bus stop problem from the Ajax and jQuery by using promises.

--- JavaScript Applications/JsApplications/AJAX and jQuery AJAX/Exercise/01_Bus_stop/solution.js
+++ JavaScript Applications/JsApplications/AJAX and jQuery AJAX/Exercise/01_Bus_stop/solution.js
@@ -2,19 +2,17 @@ function getInfo() {
     let stopName = $("#stopName");
 
     let stopId = $("#stopId").val();
-    let url = "https://testapp-3b30c.firebaseio.com/businfo/" + stopId + ".json";
+    let url = "https://judgetests.firebaseio.com/businfo/" + stopId + ".json";
 
     let bussesList = $("#buses");
 
-    $.get(url, function(stop){
-        if(stop === null){
-            stopName.text("Error");
-            bussesList.html("");
-        } else{
-            stopName.text(stop.name);    
-            Object.keys(stop.buses).forEach((k) => {
-                bussesList.append("<li>Bus " + k.toString() + " arrives in " + stop.buses[k].toString() + " minutes");
-            });
-        }
+    $.get(url).then((stop) => {
+        stopName.text(stop.name);    
+        Object.keys(stop.buses).forEach((k) => {
+            bussesList.append("<li>Bus " + k.toString() + " arrives in " + stop.buses[k].toString() + " minutes");
+        });
+    }).catch((error) => {
+        stopName.text("Error");
+        bussesList.html("");
     });
 }
